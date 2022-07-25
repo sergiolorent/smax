68183f18ab45002cb43854d921ea549d95c1e5ae
798334 <798334@unizar.es>
2022-07-25 20:17:37 +0200

    Initial commit

---
+++ back-end/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyparser = require("body-parser");
+const app = express();
+//require("dotenv").config();
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function () {
+    console.log(`Servidor escuchando en el puerto ${PORT}.`);
+});
+
+//app.use(bodyparser.urlencoded({extended:false}))
+app.use(bodyparser.json())
+app.use(express.urlencoded({extended:false}));
